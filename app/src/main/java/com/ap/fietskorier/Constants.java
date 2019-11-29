6e6a016f4df02e5743ca289114d6777c595e7f93
Amer <amer.shanawany@gmail.com>
2019-11-29 02:43:34 +0100

    Fixed maps_bug

--- app/src/main/java/com/ap/fietskorier/Constants.java
+++ app/src/main/java/com/ap/fietskorier/Constants.java
@@ -1,9 +1,13 @@
 package com.ap.fietskorier;
 
+import android.Manifest;
+
 public class Constants {
     public static final int ERROR_DIALOG_REQUEST =9001;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS =9002;
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION= 9003;
     public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
-
-}
+    public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    public static  final  int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    public static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+ }
--- app/src/main/java/com/ap/fietskorier/activity_maps.java
+++ app/src/main/java/com/ap/fietskorier/activity_maps.java
@@ -1,8 +1,14 @@
 package com.ap.fietskorier;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,10 +17,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.ap.fietskorier.Constants.COARSE_LOCATION;
+import static com.ap.fietskorier.Constants.FINE_LOCATION;
+import static com.ap.fietskorier.Constants.LOCATION_PERMISSION_REQUEST_CODE;
+
 
 public class activity_maps extends AppCompatActivity
         implements OnMapReadyCallback {
-
+    private static final String TAG = "MapActivity";
+    private boolean mLocationPermissionsGranted=false ;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +34,11 @@ public class activity_maps extends AppCompatActivity
         setContentView(R.layout.activity_maps);
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-         
+       // SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+         //       .findFragmentById(R.id.map);
+       // mapFragment.getMapAsync(this);
+        getLocationPermission();
+
     }
 
     /**
@@ -39,11 +52,57 @@ public class activity_maps extends AppCompatActivity
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Toast.makeText(this,"Map is ready ",Toast.LENGTH_SHORT).show();
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions().position(sydney)
                 .title("Marker   in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                //
+                if(grantResults.length>0){
+
+                    for (int i =0 ; i < grantResults.length;i++){
+                        if (grantResults[i]!=PackageManager.PERMISSION_GRANTED ){
+
+                            mLocationPermissionsGranted =false;
+                            return;
+                        }
+
+                    }
+                    mLocationPermissionsGranted = true;
+                    //initialize our map
+                    initMap();
+
+                }
+
+
+        }
+    }
+    private void initMap(){
+        final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(activity_maps.this);
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+mLocationPermissionsGranted = true;
+            }
+            else{
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            }
+
+        }
     }
 }
