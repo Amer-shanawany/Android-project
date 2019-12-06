6cdc54fa30dad069e8ae79c54b9079190c394810
Jonas Baert <jonas.baert@student.ap.be>
2019-12-06 14:47:17 +0100

    Jonas local changes

---
+++ app/src/main/java/com/ap/fietskorier/DeliverPackage.java
@@ -0,0 +1,14 @@
+package com.ap.fietskorier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliverPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deliver_package);
+    }
+}
---
+++ app/src/main/java/com/ap/fietskorier/PickupPackage.java
@@ -0,0 +1,14 @@
+package com.ap.fietskorier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickupPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup_package);
+    }
+}
